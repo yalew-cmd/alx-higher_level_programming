3d75a46052c56a4bc21bd33c7a919cb336126e8c
Yalew Jemberu <yalewjemberu@gmail.com>
2022-07-04 19:02:16 +0300

    First committt

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,37 +1,42 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 /**
- * is_palindrome - Check is the listint is a Palindrome
- * @head: type listint_s double pointer of node
- * return: 1 if is a pilndrome 0 if not
+ * is_palindrome- Checks if a singly linked list is a palindrome.
+ * @head: Head pointer
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
-
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL && *head == NULL)
-		return (1);
-	else
-		return (rec_palindrome(head, *head));
-}
+	listint_t *node;
+	int values[9999], i = 0, c = 0;
 
-/**
- * rec_palindrome - Recursion to check each node as palindrome
- * @head: type listint_s double pointer of node
- * @tail: type listint_s single pointer of last node
- * return: 1 if is a pilndrome 0 if not
- */
-
-int rec_palindrome(listint_t **head, listint_t *tail)
-{
-	if (tail == NULL)
-		return (1);
-
-	if (rec_palindrome(head, tail->next) && (*head)->n == tail->n)
+	if ((!*head) || (!head))
 	{
-		*head = (*head)->next;
 		return (1);
 	}
-	return (0);
+	node = *head;
+	if (!node->next)
+	{
+		return (1);
+	}
+	while (node)
+	{
+		values[i] = node->n;
+		node = node->next;
+		i++;
+	}
+	i--;
+	while (i >= 0 && c <= i)
+	{
+		if (values[i] != values[c])
+		{
+			return (0);
+		}
+		i--;
+		c++;
+	}
+	return (1);
 }
