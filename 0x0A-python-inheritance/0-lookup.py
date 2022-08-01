8f545948628ba16d082d55b08726e9b0844efa87
yalew-cmd <66673638+yalew-cmd@users.noreply.github.com>
2022-08-01 18:32:48 +0300

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""return the list of available attributes
-and methods of an object"""
-
-
-def lookup(obj):
-    """returns list of available
-        attributes and methods of an object"""
-    return dir(obj)
--- 0x0A-python-inheritance/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""public instance method"""
-
-
-class BaseGeometry:
-    """public instance method"""
-    def area(self):
-        """raises an exception"""
-        raise Exception("area() is not implemented")
