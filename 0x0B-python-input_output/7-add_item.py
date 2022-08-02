9ddcca60edefb5c26fd8b3894f8ff8e1045bbbcd
yalew-cmd <66673638+yalew-cmd@users.noreply.github.com>
2022-08-02 16:43:46 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""
-    This is a python script that adds all arguments to a Python List.
-    List is then saved to a file.
-"""
+'''
+Write a function that writes an
+Object to a text file, using a
+JSON representation
+'''
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import json
 
 
-if __name__ == "__main__":
-
-    filename = "add_item.json"
-
-    try:
-        arg_list = load_from_json_file(filename)
-    except:
-        arg_list = []
-
-    for arg in sys.argv[1:]:
-        arg_list.append(arg)
-    save_to_json_file(arg_list, filename)
+def save_to_json_file(my_obj, filename):
+    '''
+    Writes object to text file using JSON
+    '''
+    if filename is None:
+        return
+    with open(filename, 'w', encoding='utf-8') as f:
+        json_var = json.dump(my_obj, f)
