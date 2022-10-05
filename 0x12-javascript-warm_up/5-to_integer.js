c167c8df18d06c4f6e6585800dd6272b3c6cebf3
yalew-cmd <66673638+yalew-cmd@users.noreply.github.com>
2022-10-05 23:12:48 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const argsToConvert = parseInt(process.argv[2]);
-if (isNaN(argsToConvert)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + argsToConvert);
-}
